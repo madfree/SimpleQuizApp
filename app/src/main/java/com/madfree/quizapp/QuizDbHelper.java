6d07a267a811996105051011aee0a00450c8e056
mfrei <matzefrei@gmail.com>
2019-11-12 18:14:30 +0100

    handled configuration changes with onSaveInstanceState

--- app/src/main/java/com/madfree/quizapp/QuizDbHelper.java
+++ app/src/main/java/com/madfree/quizapp/QuizDbHelper.java
@@ -7,6 +7,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import com.madfree.quizapp.QuizContract.*;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -67,8 +68,8 @@ public class QuizDbHelper extends SQLiteOpenHelper {
         db.insert(QuestionsTable.TABLE_NAME, null, cv);
     }
 
-    public List<Question> getAllQuestions() {
-        List<Question> questionList = new ArrayList<>();
+    public ArrayList<Question> getAllQuestions() {
+        ArrayList<Question> questionList = new ArrayList<>();
         db = getReadableDatabase();
         Cursor c = db.rawQuery("SELECT * FROM " + QuestionsTable.TABLE_NAME, null);
 
